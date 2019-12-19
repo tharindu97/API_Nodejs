f5eeee95806a54c8da3d4af91bf42f66842eb3a3
tharindu97 <tharindukavishna@gmail.com>
2019-12-19 15:42:22 +0530

    completed_3

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,8 +5,17 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET request to /products'
+    Product.find()
+    .exec()
+    .then(docs =>{
+        console.log(docs);
+        res.status(200).json(docs);
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
@@ -19,14 +28,21 @@ router.post('/', (req, res, next) => {
     product.save()
     .then(result => {
         console.log(result);
+        res.status(201).json({
+            message: 'Handling POST request to /products',
+            createProduct: result
+            //product: product
+        });
     })
-    .catch(err => console.log(err));
-
-
-    res.status(201).json({
-        message: 'Handling POST request to /products',
-        product: product
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
+
+
+    
 });
 
 router.get('/:productId', (req, res, next) => {
@@ -34,8 +50,12 @@ router.get('/:productId', (req, res, next) => {
     Product.findById(id)
     .exec()
     .then(doc =>{
-        console.log(doc);
-        res.status(200).json(doc);
+        console.log("From Database",doc);
+        if(doc){
+            res.status(200).json(doc);
+        }else{
+            res.status(404).json({message:'No valid entry found for ID'});
+        }
     })
     .catch(err => {
         console.log(err);
