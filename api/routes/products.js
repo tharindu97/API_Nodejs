1b70f670ab23d2f89261b3e0763e2f8c17a190e8
tharindu97 <tharindukavishna@gmail.com>
2020-01-16 11:46:54 +0530

    6_completed

--- api/routes/products.js
+++ api/routes/products.js
@@ -60,15 +60,45 @@ router.get('/:productId', (req, res, next) => {
 });
 
 router.patch('/:productId', (req, res, next) => {
+
+    const id = req.params.productId;
+    const updateOps = {};
+    for (const ops of req.body){
+        updateOps[ops.proName] = ops.value;
+    }
+    Product.update({ _id: id}, {$set: updateOps})
+    .exec()
+    .then(result => {
+        console.log(result);
+        res.status(200).json(result);
+    })
+    .catch( err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    })
     res.status(200).json({
         message:'Updated Products!'
     });
 });
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
+    const id = req.params.productId;
+    Product.remove({ _id: id})
+    .exec()
+    .then(result =>{
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    })
+    /*res.status(200).json({
         message:'Deleted Products!'
-    });
+    });*/
 });
 
 
