1f991afd046612dbffe457152bf2b72b297dd4f7
tharindu97 <tharindukavishna@gmail.com>
2020-03-15 21:19:33 +0530

    create_router

--- API02/index.js
+++ API02/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 //Set up express app
 const app = express();
 
+//initialize routes
+app.use('/api', require('./routes/api'));
+
 //Listen for request
 app.listen(process.env.port || 4000, function() {
     console.log('connect');
---
+++ API02/routes/api.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+//get a list of about from the db
+router.get('/about', function(req, res){
+    res.send({type: 'GET'});
+});
+
+//add a new about from the db
+router.post('/about', function(req, res){
+    res.send({type: 'POST'});
+});
+
+//update a about from the db
+router.put('/about/:id', function(req, res){
+    res.send({type: 'PUT'});
+});
+
+//delete a about from the db
+router.delete('/about/:id', function(req, res){
+    res.send({type: 'GET'});
+});
+
+module.exports = router;
